d233d5892863aa5834aae219f1bb8de4471eaa78
yanas-logs <81765889+yanas-logs@users.noreply.github.com>
2025-11-16 23:23:35 +0000

    chore: implement nativewind

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,21 +1,11 @@
-import { StyleSheet, Text, View } from "react-native";
-
+import { Text, View } from "react-native";
+ 
 export default function IndexScreen() {
   return (
-    <View className="flex-1 justify-center items-center bg-white">
-      <Text className="text-lg font-semibold">Home Screen</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome!
+      </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    text: {
-        fontSize: 22,
-        fontWeight: "600",
-    },
-});
