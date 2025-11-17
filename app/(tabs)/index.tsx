ff1bbc3bc39fbcbf78045aba2e2624d718ebc4c0
yanas-logs <81765889+yanas-logs@users.noreply.github.com>
2025-11-17 00:37:14 +0000

    chore: refactor _layout.tsx app and index.tsx app(tabs)

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,33 @@
-import { Text, View } from "react-native";
- 
+import { offers } from "@/constants";
+import React, { Fragment } from "react";
+import { FlatList, Pressable, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
 export default function IndexScreen() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome!
-      </Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white px-4 pt-4">
+      <FlatList
+        data={offers}
+        keyExtractor={(item) => item.id.toString()}
+        contentContainerClassName="pb-20"
+        renderItem={({ item }) => (
+          <View className="mb-3">
+            <Pressable
+              className="offer-card"
+              style={{ backgroundColor: item.color }}
+              android_ripple={{ color: '#ffffff22' }}
+            >
+              {({ pressed }) => (
+                <Fragment>
+                  <View>
+
+                  </View>
+                </Fragment>
+              )}
+            </Pressable>
+          </View>
+        )}
+      />
+    </SafeAreaView>
   );
 }
